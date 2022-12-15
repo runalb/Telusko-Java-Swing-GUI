d981fba5e7cc3575cc6e764e2947cbb9c7058fa8
runalb <65166862+runalb@users.noreply.github.com>
2022-12-15 18:43:33 +0530

    3 - First Code in Java Swing GUI Part 2 Layout

--- src/FirstGUI.java
+++ src/FirstGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class FirstGUI {
     public static void main(String args[]){
@@ -9,11 +10,15 @@ public class FirstGUI {
 
 class Abc extends JFrame{
     public Abc(){
-        JLabel l = new JLabel("Hello World");
-        add(l);
-
+        this.setLayout(new FlowLayout());
         this.setVisible(true);
         this.setSize(400,400);
         this.setTitle("My Frame");
+
+        JLabel l = new JLabel("Hello World");
+        add(l);
+
+        JLabel l2 = new JLabel("Welcome");
+        add(l2);
     }
 }
